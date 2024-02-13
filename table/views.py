6c29ca850255ec64359ff9250cdef72be982c866
AngelinaRepina <104089541+AngelinaRepina@users.noreply.github.com>
2024-02-13 11:04:34 +0300

    ..

--- table/views.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from django.shortcuts import render
-
-
-def index(request):
-    data = datetime.now()
-    print(f'Привет мир в консоли {data}')
-    return render(request, 'index.html', context={'index_val_1': data})
